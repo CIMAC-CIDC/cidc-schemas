9bd5a0772c12b95f5736fb1dc565045ef776b060
James Lindsay <jlindsay@jimmy.harvard.edu>
2019-07-01 09:57:34 -0400

    connected rnaseq assay to artifacts (#83)

--- tests/test_assays.py
+++ tests/test_assays.py
@@ -60,7 +60,8 @@ def test_wes():
 
   # create the wes object
   fastq_1 = ARTIFACT_OBJ.copy()
-  fastq_1['info'] = {"pizza": "bagel"}
+  rgmf = ARTIFACT_OBJ.copy()
+  rgmf['artifact_category'] = 'Assay Artifact from CIMAC'
   record = {
     "enrichment_vendor_kit": "Twist",
     "enrichment_vendor_lot": "dummy_value", 
@@ -74,7 +75,7 @@ def test_wes():
         "fastq_1": fastq_1,
         "fastq_2": fastq_1
       },
-      "read_group_mapping_file": fastq_1
+      "read_group_mapping_file": rgmf
     }
   }
 
@@ -94,3 +95,52 @@ def test_wes():
     validator.validate(obj)
 
 
+def test_rna_expression():
+
+  # create the ngs object
+  ngs_obj = {
+    "sequencer_platform": "Illumina - NovaSeq 6000",
+    "library_vendor_kit": "KAPA - Hyper Prep",
+    "library_kit_lot": "dummy_value",
+    "library_prep_date": "01/01/2001",
+    "paired_end_reads": "Paired",
+    "read_length": 200,
+    "input_ng": 666,
+    "library_yield_ng": 666,
+    "average_insert_size": 200
+  }
+  obj = {**ASSAY_CORE, **ngs_obj}    # merge two dictionaries
+
+  # add custom entry
+  obj['enrichment_method'] = "Ribo minus"
+
+  # create the wes object
+  fastq_1 = ARTIFACT_OBJ.copy()
+  rgmf = ARTIFACT_OBJ.copy()
+  rgmf['artifact_category'] = 'Assay Artifact from CIMAC'
+  record = {
+    "enrichment_vendor_kit": "Twist",
+    "enrichment_vendor_lot": "dummy_value", 
+    "capture_date": "01/01/2001",
+    "files": {
+        "fastq_1": fastq_1,
+        "fastq_2": fastq_1,
+        "read_group_mapping_file": rgmf
+      }
+  }
+  
+
+  # add a demo record.
+  obj['records'] = [
+    record
+  ]
+  
+
+  # create validator assert schemas are valid.
+  validator = _fetch_validator("rna_expression")
+  validator.validate(obj)
+
+  # assert negative behaviors
+  del obj['records'][0]['enrichment_vendor_kit']
+  with pytest.raises(jsonschema.ValidationError):
+    validator.validate(obj)

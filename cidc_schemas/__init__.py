ac3d6f5f04b612ab8470333febe41b8a1ed35a56
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-07-11 10:00:03 -0400

    Add SCHEMA_LIST as a constant

--- cidc_schemas/__init__.py
+++ cidc_schemas/__init__.py
@@ -5,3 +5,12 @@
 __author__ = """James Lindsay"""
 __email__ = 'jlindsay@jimmy.harvard.edu'
 __version__ = '0.1.0'
+
+from .template import Template
+
+
+def validate_xlsx(xlsx_path: str, schema_path: str, raise_validation_errors: bool = True):
+    """Check if a populated .xlsx template file is valid w.r.t. the schema at schema_path"""
+    template = Template.from_json(schema_path)
+    validation = template.validate_excel(xlsx_path, raise_validation_errors)
+    return validation
--- cidc_schemas/constants.py
+++ cidc_schemas/constants.py
@@ -1,3 +1,6 @@
 import os
 
-SCHEMA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "schemas")
+SCHEMA_DIR = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "schemas")
+SCHEMA_LIST = [f'{d}/{f}'.replace(f'{SCHEMA_DIR}/', '')
+               for d, _, fs in os.walk(SCHEMA_DIR) for f in fs]

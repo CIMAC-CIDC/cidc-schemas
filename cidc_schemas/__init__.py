14a70f657cb187fa2b3812f27878224f6505b93e
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-07-14 11:17:14 -0400

    Release changes on master (#102)
    * Add installation from GitHub and release branch status (#93)
    
    * Last bigone (#94)
    
    * modified schema to embed assay into aliquot
    
    * fixed typo
    
    * renamed property in aliquot assays to assay to reflect usage
    
    * Include schemas in the cidc_schemas package (#95)
    
    * modified schema to embed assay into aliquot
    
    * fixed typo
    
    * renamed property in aliquot assays to assay to reflect usage
    
    * Move schemas inside cidc_schemas
    
    * Include schemas in distribution
    
    * Update schema path logic to handle new location
    
    * Update CLI commands in README
    
    * Move schemas into cidc_schemas
    
    * rename SCHEMA_ROOT to SCHEMA_DIR
    
    * Add command to list all schemas from CLI
    
    * API updates (#97)
    
    * Add SCHEMA_LIST as a constant
    
    * Add option to get validation errors as a list of strings
    
    * Convert assertions to validation errors
    
    * Update type hints to reflect openpyxl loader interface (#100)
    
    * Remove unused or unnecessary requirements (#101)

--- cidc_schemas/__init__.py
+++ cidc_schemas/__init__.py
@@ -6,11 +6,13 @@ __author__ = """James Lindsay"""
 __email__ = 'jlindsay@jimmy.harvard.edu'
 __version__ = '0.1.0'
 
+from typing import Union, BinaryIO
+
 from .template import Template
 
 
-def validate_xlsx(xlsx_path: str, schema_path: str, raise_validation_errors: bool = True):
+def validate_xlsx(xlsx: Union[str, BinaryIO], schema_path: str, raise_validation_errors: bool = True):
     """Check if a populated .xlsx template file is valid w.r.t. the schema at schema_path"""
     template = Template.from_json(schema_path)
-    validation = template.validate_excel(xlsx_path, raise_validation_errors)
+    validation = template.validate_excel(xlsx, raise_validation_errors)
     return validation

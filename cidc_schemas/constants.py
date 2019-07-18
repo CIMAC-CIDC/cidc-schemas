d1b8c737eaf5e6339deaaffd0b01eb7f8c4f76fe
James Lindsay <jlindsay@jimmy.harvard.edu>
2019-07-18 10:56:58 -0400

    added first prototype of filegen_wes (#107)

--- cidc_schemas/constants.py
+++ cidc_schemas/constants.py
@@ -4,3 +4,5 @@ SCHEMA_DIR = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "schemas")
 SCHEMA_LIST = [f'{d}/{f}'.replace(f'{SCHEMA_DIR}/', '')
                for d, _, fs in os.walk(SCHEMA_DIR) for f in fs]
+
+TEMPLATE_DIR = os.path.join(SCHEMA_DIR, 'templates')

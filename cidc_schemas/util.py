d33c55ee41fda5e5af8cbded47569a8bafdbaffb
James Lindsay <jlindsay@jimmy.harvard.edu>
2019-09-04 15:51:30 -0400

    added utility function to parse npx files and added specific artifact… (#158)
    * added utility function to parse npx files and added specific artifact to hold detailed information
    
    * cleaned up docstring, added failure mode tests
    
    * version bump

--- cidc_schemas/util.py
+++ cidc_schemas/util.py
@@ -1,10 +1,13 @@
 import os
 import json
 import yaml
+import openpyxl
+from typing import Union, BinaryIO, List
+
 
 def yaml_to_json(yaml_path: str) -> str:
     """
-        Given a path to a yaml file, write the equivalent json 
+        Given a path to a yaml file, write the equivalent json
         to a file of the same name and return the new filename.
     """
     filename, ext = yaml_path.rsplit('.', 1)
@@ -34,3 +37,52 @@ def json_to_yaml(json_path: str) -> str:
             yaml.safe_dump(dictionary, new_file)
 
     return yaml_path
+
+
+def parse_npx(xlsx_path: Union[str, BinaryIO]) -> List[str]:
+    """
+    Parses the given NPX file from OLINK
+    to extracts a list of aliquot IDs. If the file 
+    is not valid NPX but still xlsx the function will return an empty
+    list. The function will pass along any IO errors.
+
+    Args:
+        xlsx_path: path to NPX file on disk, or an opened NPX file
+
+    Returns:
+        arg1: a list of IDs found in this file
+    """
+
+    # load the file
+    workbook = openpyxl.load_workbook(xlsx_path)
+
+    # extract data to python
+    ids = []
+    for worksheet_name in workbook.sheetnames:
+
+        # simplify.
+        worksheet = workbook[worksheet_name]
+        seen_onlinkid = False
+        for i, row in enumerate(worksheet.iter_rows()):
+
+            # extract values from row
+            vals = [col.value for col in row]
+
+            # skip empty
+            if len(vals) == 0 or vals[0] is None:
+                continue
+
+            # check if we are starting ids
+            if vals[0] == 'OlinkID':
+                seen_onlinkid = True
+                continue
+
+            # check if we are done.
+            if vals[0] == 'LOD':
+                break
+
+            # get the identifier
+            if seen_onlinkid:
+                ids.append(vals[0])
+
+    return ids

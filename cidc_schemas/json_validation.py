2b636438d6d2a21c4f883837d00341214f853106
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-09-06 08:14:18 -0400

    wip

--- cidc_schemas/json_validation.py
+++ cidc_schemas/json_validation.py
@@ -21,7 +21,7 @@ def load_and_validate_schema(
     """
     Try to load a valid schema at `schema_path`. If an `on_refs` function
     is supplied, call that on all refs in the schema, rather than
-    resolving the refs. Note: it is shallow, i.e., if calling `on_refs` on a node produces 
+    resolving the refs. Note: it is shallow, i.e., if calling `on_refs` on a node produces
     a new node that contains refs, those refs will not be resolved.
 
     If return validator is true it will return
@@ -57,31 +57,36 @@ def _map_refs(node: dict, on_refs: Callable[[str], dict]) -> dict:
     Apply `on_refs` to all nodes with `$ref`, returning node with refs replaced
     with results of the function call.
 
-    Note: _map_refs is shallow, i.e., if calling `on_refs` on a node produces 
+    Note: _map_refs is shallow, i.e., if calling `on_refs` on a node produces
     a new node that contains refs, those refs will not be resolved.
     """
-    if isinstance(node, collections.abc.Mapping) and '$ref' in node:
-        extra_keys = set(node.keys()).difference({'$ref', '$comment'})
-        if extra_keys:
-            # As for json-schema.org:
-            # "... You will always use $ref as the only key in an object: 
-            # any other keys you put there will be ignored by the validator."
-            # So we raise on that, to notify schema creator that s/he should not 
-            # expect those additional keys to be verified by schema validator.
-            raise Exception(f"Schema node with '$ref' should not contain anything else (besides '$comment' for docs). \
-                \nOn: {node} \nOffending keys {extra_keys}")
-        
-        # We found a ref, so return it mapped through `on_refs`
-        new_node = on_refs(node['$ref'])
-        # Plus concatenated new and old '$comment' fields 
-        # which should be just ignored anyways.
-        if '$comment' in new_node or '$comment' in node:
-            new_node['$comment'] = new_node.get('$comment', '') + node.get('$comment', '')
-        return new_node
-    elif isinstance(node, collections.abc.Mapping):
-        # Look for all refs in this mapping
-        for k, v in node.items():
-            node[k] = _map_refs(v, on_refs)
+    if isinstance(node, collections.abc.Mapping):
+        if '$ref' in node or 'type_ref' in node:
+            if '$ref' in node:
+                ref_key = '$ref'
+                extra_keys = set(node.keys()).difference({'$ref', '$comment'})
+                if extra_keys:
+                    # As for json-schema.org:
+                    # "... You will always use $ref as the only key in an object:
+                    # any other keys you put there will be ignored by the validator."
+                    # So we raise on that, to notify schema creator that s/he should not
+                    # expect those additional keys to be verified by schema validator.
+                    raise Exception(f"Schema node with '$ref' should not contain anything else (besides '$comment' for docs). \
+                        \nOn: {node} \nOffending keys {extra_keys}")
+            else:
+                ref_key = 'type_ref'
+            # We found a ref, so return it mapped through `on_refs`
+            new_node = on_refs(node[ref_key])
+            # Plus concatenated new and old '$comment' fields
+            # which should be just ignored anyways.
+            if '$comment' in new_node or '$comment' in node:
+                new_node['$comment'] = new_node.get(
+                    '$comment', '') + node.get('$comment', '')
+            return new_node
+        else:
+            # Look for all refs in this mapping
+            for k, v in node.items():
+                node[k] = _map_refs(v, on_refs)
     elif isinstance(node, (list, tuple)):
         # Look for all refs in this list
         for i in range(len(node)):
@@ -96,7 +101,7 @@ def _resolve_refs(base_uri: str, json_spec: dict) -> dict:
     """
     resolver = jsonschema.RefResolver(base_uri, json_spec)
 
-    def _resolve_ref(ref:str) -> dict:
+    def _resolve_ref(ref: str) -> dict:
         with resolver.resolving(ref) as resolved_spec:
             # resolved_spec might have unresolved refs in it, so we pass
             # it back to _resolve_refs to resolve them. This way,
@@ -125,13 +130,14 @@ def validate_instance(instance: str, schema: dict, required: bool) -> Optional[s
             stype = schema.get('type')
         if not stype:
             if 'allOf' in schema:
-                types = set(s.get('type') for s in schema['allOf'] if 'type' in s)
+                types = set(s.get('type')
+                            for s in schema['allOf'] if 'type' in s)
                 # if all types in 'allOf' are the same:
                 if len(types) == 1:
                     stype = types.pop()
                 else:
                     return f"Value can't be of multiple different types ({types}), "\
-                    "as 'allOf' in schema specifies."
+                        "as 'allOf' in schema specifies."
 
         instance = convert(stype, instance)
 
@@ -160,6 +166,7 @@ def _to_time(value):
     if not dt:
         raise ValueError(f"could not convert \"{value}\" to time")
     return dt.strftime('%H:%M:%S')
+
 
 def _to_datetime(value):
     dt = _get_datetime(value)

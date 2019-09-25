b19b0cf1ce54d29964e7ba6c88bede3bd1505e73
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-09-25 13:37:56 -0400

    Ensure unprism test data is valid

--- tests/test_unprism.py
+++ tests/test_unprism.py
@@ -1,4 +1,11 @@
 from cidc_schemas.unprism import unprism_participants, unprism_samples
+from cidc_schemas.json_validation import load_and_validate_schema
+
+
+def _validate_ct(ct: dict):
+    validator = load_and_validate_schema(
+        'clinical_trial.json', return_validator=True)
+    validator.validate(ct)
 
 
 def test_unprism_participants():
@@ -11,7 +18,7 @@ def test_unprism_participants():
                 "gender": "Male",
                 "samples": [],
                 "cohort_id": "54321",
-                "cimac_participant_id": "PA1",
+                "cimac_participant_id": "CM-TEST-PA11",
                 "trial_participant_id": "tPA1"
             },
             {
@@ -20,17 +27,21 @@ def test_unprism_participants():
                 "arm_id": "12345",
                 "samples": [],
                 "cohort_id": "54321",
-                "cimac_participant_id": "PA2",
+                "cimac_participant_id": "CM-TEST-PA12",
                 "trial_participant_id": "tPA2"
             }
         ],
     }
 
+    # Validate the trial_metadata object to ensure this test fails
+    # if the data model changes.
+    _validate_ct(trial_metadata)
+
     csv = unprism_participants(trial_metadata)
     expected = """,arm_id,gender,cohort_id,cimac_participant_id,trial_participant_id,race,lead_organization_study_id
-        0,12345,Male,54321,PA1,tPA1,,test_trial_id
-        1,12345,Other,54321,PA2,tPA2,White,test_trial_id
-    """.replace(" ", "")
+0,12345,Male,54321,CM-TEST-PA11,tPA1,,test_trial_id
+1,12345,Other,54321,CM-TEST-PA12,tPA2,White,test_trial_id
+"""
     assert csv == expected
 
 
@@ -39,14 +50,22 @@ def test_unprism_samples():
     Check that we can extract sample-level data from a trial metadata object with multiple
     participants and different properties in different samples.
     """
+
+    extra_participant_fields = {
+        "trial_participant_id": "",
+        "cohort_id": "",
+        "arm_id": "",
+    }
+
     trial_metadata = {
         "lead_organization_study_id": "1/minimal",
         "participants": [
             {
-                "cimac_participant_id": "PA.1.1",
+                "cimac_participant_id": "CM-TEST-PA11",
+                **extra_participant_fields,
                 "samples": [
                     {
-                        "cimac_id": "SA.1.1.1",
+                        "cimac_id": "CM-TEST-PA11-S1",
                         "site_sample_id": "site.SA.1.1.1",
                         "time_point": "---",
                         "sample_location": "---",
@@ -56,32 +75,39 @@ def test_unprism_samples():
                         "aliquots": []
                     },
                     {
-                        "cimac_id": "SA.1.1.2",
+                        "cimac_id": "CM-TEST-PA11-S2",
                         "site_sample_id": "site.SA.1.1.2",
                         "time_point": "---",
-                        "specimen_type": "plasma",
+                        "sample_location": "---",
+                        "specimen_format": "FFPE Block",
+                        "specimen_type": "Plasma",
+                        "genomic_source": "Tumor",
                         "aliquots": []
                     }
                 ]
             },
             {
-                "cimac_participant_id": "PA.1.2",
+                "cimac_participant_id": "CM-TEST-PA12",
+                **extra_participant_fields,
                 "samples": [
                     {
-                        "cimac_id": "SA.1.2.1",
+                        "cimac_id": "CM-TEST-PA12-S1",
                         "site_sample_id": "site.SA.1.2.1",
                         "time_point": "---",
                         "sample_location": "---",
                         "specimen_type": "Other",
-                        "specimen_format": "Other",
+                        "specimen_format": "EDTA Tube",
                         "genomic_source": "Normal",
                         "aliquots": []
                     },
                     {
-                        "cimac_id": "SA.1.2.2",
+                        "cimac_id": "CM-TEST-PA12-S2",
                         "site_sample_id": "site.SA.1.2.2",
                         "time_point": "---",
-                        "specimen_type": "pbmc",
+                        "sample_location": "---",
+                        "specimen_type": "PBMC",
+                        "specimen_format": "EDTA Tube",
+                        "genomic_source": "Tumor",
                         "aliquots": []
                     }
                 ]
@@ -89,13 +115,17 @@ def test_unprism_samples():
         ]
     }
 
+    # Validate the trial_metadata object to ensure this test fails
+    # if the data model changes.
+    _validate_ct(trial_metadata)
+
     csv = unprism_samples(trial_metadata)
 
     expected = """,cimac_id,site_sample_id,time_point,sample_location,specimen_type,specimen_format,genomic_source,lead_organization_study_id,participants.cimac_participant_id
-        0,SA.1.1.1,site.SA.1.1.1,---,---,Other,Other,Normal,1/minimal,PA.1.1
-        1,SA.1.1.2,site.SA.1.1.2,---,,plasma,,,1/minimal,PA.1.1
-        2,SA.1.2.1,site.SA.1.2.1,---,---,Other,Other,Normal,1/minimal,PA.1.2
-        3,SA.1.2.2,site.SA.1.2.2,---,,pbmc,,,1/minimal,PA.1.2
-    """.replace(" ", "")
+0,CM-TEST-PA11-S1,site.SA.1.1.1,---,---,Other,Other,Normal,1/minimal,CM-TEST-PA11
+1,CM-TEST-PA11-S2,site.SA.1.1.2,---,---,Plasma,FFPE Block,Tumor,1/minimal,CM-TEST-PA11
+2,CM-TEST-PA12-S1,site.SA.1.2.1,---,---,Other,EDTA Tube,Normal,1/minimal,CM-TEST-PA12
+3,CM-TEST-PA12-S2,site.SA.1.2.2,---,---,PBMC,EDTA Tube,Tumor,1/minimal,CM-TEST-PA12
+"""
 
     assert csv == expected

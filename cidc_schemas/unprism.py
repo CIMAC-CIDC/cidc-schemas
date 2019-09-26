d884a814deda3814db66cf7a43805f434c4bba78
James Lindsay <jlindsay@jimmy.harvard.edu>
2019-09-26 12:50:11 -0400

    resolved unprism errors with new DM

--- cidc_schemas/unprism.py
+++ cidc_schemas/unprism.py
@@ -4,9 +4,9 @@ from pandas.io.json import json_normalize
 def unprism_participants(trial_metadata: dict):
     """Return a CSV of patient-level metadata for the given trial."""
     participants = json_normalize(data=trial_metadata, record_path=['participants'],
-                                  meta=['lead_organization_study_id'])
+                                  meta=['protocol_id'])
 
-    participants.drop('samples', axis=1, inplace=True)
+    participants.drop('samples', axis=1, inplace=True, errors='ignore')
 
     return participants.to_csv(index=False)
 
@@ -14,8 +14,8 @@ def unprism_participants(trial_metadata: dict):
 def unprism_samples(assay_type: str):
     """Return a CSV of patient-level metadata for the given trial."""
     samples = json_normalize(data=assay_type, record_path=['participants', "samples"],
-                             meta=["lead_organization_study_id", ["participants", "cimac_participant_id"]])
+                             meta=["protocol_id", ["participants", "cimac_participant_id"]])
 
-    samples.drop('aliquots', axis=1, inplace=True)
+    samples.drop('aliquots', axis=1, inplace=True, errors='ignore')
 
     return samples.to_csv(index=False)

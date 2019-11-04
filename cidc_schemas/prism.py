69204b348c28dc38a0ab617c144b476f80572541
Roshni <rbiswas@ds.dfci.harvard.edu>
2019-11-04 15:27:56 -0500

    CyTOF Support: CyTOF Analysis Template (#216)
    * created `cytof_analysis_template`
    * added tests for `cytof_analysis_template`
    * updated cytof xlsx templates
    * bumped __version__ = '0.9.5'
    * converged on test_prism_trial_id as the example template protocol identifier

--- cidc_schemas/prism.py
+++ cidc_schemas/prism.py
@@ -239,7 +239,8 @@ def _get_recursively(search_dict, field):
 
 SUPPORTED_ASSAYS = ["wes", "olink", "cytof", "ihc"]
 SUPPORTED_MANIFESTS = ["pbmc", "plasma"]
-SUPPORTED_TEMPLATES = SUPPORTED_ASSAYS + SUPPORTED_MANIFESTS
+SUPPORTED_ANALYSES = ["cytof_analysis"]
+SUPPORTED_TEMPLATES = SUPPORTED_ASSAYS + SUPPORTED_MANIFESTS + SUPPORTED_ANALYSES
 
 LocalFileUploadEntry = namedtuple('LocalFileUploadEntry',
                                   ["local_path", "gs_key", "upload_placeholder", "metadata_availability"])

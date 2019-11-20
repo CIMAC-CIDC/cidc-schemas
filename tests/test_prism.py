cc6a23b1b23dbbd49fc7118676706af2c73887ad
Pavel Trukhanov <pavel@ds.dfci.harvard.edu>
2019-11-20 13:34:51 -0500

    prism fix for explicit "normal_" in dna template names

--- tests/test_prism.py
+++ tests/test_prism.py
@@ -764,11 +764,11 @@ def test_end_to_end_prismify_merge_artifact_merge(xlsx, template):
             assert len(prism_patch["participants"][0]["samples"]) == 3
             assert "aliquots" not in prism_patch["participants"][0]["samples"][0]
 
-        elif template.type == "blood_dna":
+        elif template.type == "normal_blood_dna":
             assert len(prism_patch["participants"]) == 2
             assert len(prism_patch["participants"][0]["samples"]) == 3
 
-        elif template.type == "tissue_dna":
+        elif template.type == "normal_tissue_dna":
             assert len(prism_patch["participants"]) == 2
             assert len(prism_patch["participants"][0]["samples"]) == 3
 

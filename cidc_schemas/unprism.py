99b0eb19ab566c942bc82daa1e9d683475b19ffb
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-01-09 15:19:42 -0500

    Create unprism.derive_files entrypoint and refactor participant/sample CSV generation

--- cidc_schemas/unprism.py
+++ cidc_schemas/unprism.py
@@ -1,29 +1,68 @@
 """Tools from extracting information from trial metadata blobs."""
+from io import StringIO
+from typing import Callable, NamedTuple, Optional
 
 from pandas.io.json import json_normalize
 
-from .prism import PROTOCOL_ID_FIELD_NAME
+from . import prism
 
 
-def unprism_participants(trial_metadata: dict):
-    """Return a CSV of patient-level metadata for the given trial."""
-    participants = json_normalize(
-        data=trial_metadata, record_path=["participants"], meta=[PROTOCOL_ID_FIELD_NAME]
-    )
-
-    participants.drop("samples", axis=1, inplace=True, errors="ignore")
-
-    return participants.to_csv(index=False)
+class DeriveFilesContext(NamedTuple):
+    fetch_artifact: Callable[[str], Optional[StringIO]]
+    save_artifact: Callable[[str, StringIO], None]
 
 
-def unprism_samples(trial_metadata: str):
-    """Return a CSV of patient-level metadata for the given trial."""
-    samples = json_normalize(
-        data=trial_metadata,
-        record_path=["participants", "samples"],
-        meta=[PROTOCOL_ID_FIELD_NAME, ["participants", "cimac_participant_id"]],
-    )
+class FileDerivation:
+    def run(self, trial_metadata: dict, context: DeriveFilesContext):
+        """Execute the file derivation."""
+        raise NotImplementedError("No derivation implemented for this upload type.")
 
-    samples.drop("aliquots", axis=1, inplace=True, errors="ignore")
 
-    return samples.to_csv(index=False)
+def derive_files(trial_metadata: dict, upload_type: str, context: DeriveFilesContext):
+    """Derive files from a trial_metadata blob given an `upload_type`"""
+    # Select the derivation for this upload type
+    derivation: FileDerivation = FileDerivation()
+    if upload_type in prism.SUPPORTED_SHIPPING_MANIFESTS:
+        derivation = ShippingManifestDerivation()
+
+    # Run the derivation
+    derivation.run(trial_metadata, context)
+
+
+class ShippingManifestDerivation(FileDerivation):
+    """Derive files from a shipping manifest upload."""
+
+    def run(self, trial_metadata: dict, context: DeriveFilesContext):
+        trial_id = trial_metadata[prism.PROTOCOL_ID_FIELD_NAME]
+        participants_csv = self._participants_csv(trial_metadata)
+        samples_csv = self._samples_csv(trial_metadata)
+
+        context.save_artifact(f"{trial_id}/participants.csv", participants_csv)
+        context.save_artifact(f"{trial_id}/samples.csv", samples_csv)
+
+    def _participants_csv(self, trial_metadata: dict) -> StringIO:
+        """Return a CSV of patient-level metadata for the given trial."""
+        participants = json_normalize(
+            data=trial_metadata,
+            record_path=["participants"],
+            meta=[prism.PROTOCOL_ID_FIELD_NAME],
+        )
+
+        participants.drop("samples", axis=1, inplace=True, errors="ignore")
+
+        return StringIO(participants.to_csv(index=False))
+
+    def _samples_csv(self, trial_metadata: dict) -> StringIO:
+        """Return a CSV of patient-level metadata for the given trial."""
+        samples = json_normalize(
+            data=trial_metadata,
+            record_path=["participants", "samples"],
+            meta=[
+                prism.PROTOCOL_ID_FIELD_NAME,
+                ["participants", "cimac_participant_id"],
+            ],
+        )
+
+        samples.drop("aliquots", axis=1, inplace=True, errors="ignore")
+
+        return StringIO(samples.to_csv(index=False))
--- tests/test_unprism.py
+++ tests/test_unprism.py
@@ -1,9 +1,10 @@
 import os
 import json
+from io import StringIO
 
 import pytest
 
-from cidc_schemas.unprism import unprism_participants, unprism_samples
+from cidc_schemas.unprism import ShippingManifestDerivation, DeriveFilesContext
 from cidc_schemas.prism import PROTOCOL_ID_FIELD_NAME
 
 ct_example_path = os.path.join(
@@ -17,29 +18,28 @@ def ct():
         yield json.load(ct)
 
 
-def test_unprism_participants(ct):
-    """Check that we can extract patient-level data from a trial metadata object."""
-    csv = unprism_participants(ct)
+def test_shipping_manifest_derivation(ct):
+    """Check that participants and samples CSVs are derived as expected."""
 
-    expected = (
-        f"cimac_participant_id,participant_id,cohort_name,{PROTOCOL_ID_FIELD_NAME}\n"
-        "CTTTPP1,trial.PA.1,Arm_Z,10021\n"
-        "CTTTPP2,trial.PA.2,Arm_Z,10021\n"
+    def fake_save_artifact(url: str, csv: StringIO):
+        if url == "10021/participants.csv":
+            assert csv.read() == (
+                f"cimac_participant_id,participant_id,cohort_name,{PROTOCOL_ID_FIELD_NAME}\n"
+                "CTTTPP1,trial.PA.1,Arm_Z,10021\n"
+                "CTTTPP2,trial.PA.2,Arm_Z,10021\n"
+            )
+        elif url == "10021/samples.csv":
+            assert csv.read() == (
+                f"cimac_id,parent_sample_id,collection_event_name,sample_location,type_of_sample,type_of_primary_container,{PROTOCOL_ID_FIELD_NAME},participants.cimac_participant_id\n"
+                "CTTTPP1S1.00,SA.1.1,Baseline,---,Other,Other,10021,CTTTPP1\n"
+                "CTTTPP1S2.00,SA.1.2,Baseline,---,Other,Other,10021,CTTTPP1\n"
+                "CTTTPP2S1.00,SA.2.1,Baseline,---,Other,Other,10021,CTTTPP2\n"
+                "CTTTPP2S2.00,SA.2.2,Baseline,---,Other,Other,10021,CTTTPP2\n"
+            )
+        else:
+            assert 0, f"Unexpected participants/samples CSV url: {url}"
+
+    derivation = ShippingManifestDerivation()
+    derivation(
+        ct, DeriveFilesContext(fetch_artifact=None, save_artifact=fake_save_artifact)
     )
-
-    assert csv == expected
-
-
-def test_unprism_samples(ct):
-    """Check that we can extract sample-level data from a trial metadata object."""
-    csv = unprism_samples(ct)
-
-    expected = (
-        f"cimac_id,parent_sample_id,collection_event_name,sample_location,type_of_sample,type_of_primary_container,{PROTOCOL_ID_FIELD_NAME},participants.cimac_participant_id\n"
-        "CTTTPP1S1.00,SA.1.1,Baseline,---,Other,Other,10021,CTTTPP1\n"
-        "CTTTPP1S2.00,SA.1.2,Baseline,---,Other,Other,10021,CTTTPP1\n"
-        "CTTTPP2S1.00,SA.2.1,Baseline,---,Other,Other,10021,CTTTPP2\n"
-        "CTTTPP2S2.00,SA.2.2,Baseline,---,Other,Other,10021,CTTTPP2\n"
-    )
-
-    assert csv == expected

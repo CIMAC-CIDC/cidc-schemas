ba89314c9e913cdc8c10760d483116cb3ad67e8d
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-01-10 10:01:49 -0500

    Update test

--- tests/test_unprism.py
+++ tests/test_unprism.py
@@ -21,15 +21,15 @@ def ct():
 def test_shipping_manifest_derivation(ct):
     """Check that participants and samples CSVs are derived as expected."""
 
-    def fake_save_artifact(url: str, csv: StringIO):
+    def fake_save_artifact(url: str, csv: str):
         if url == "10021/participants.csv":
-            assert csv.read() == (
+            assert csv == (
                 f"cimac_participant_id,participant_id,cohort_name,{PROTOCOL_ID_FIELD_NAME}\n"
                 "CTTTPP1,trial.PA.1,Arm_Z,10021\n"
                 "CTTTPP2,trial.PA.2,Arm_Z,10021\n"
             )
         elif url == "10021/samples.csv":
-            assert csv.read() == (
+            assert csv == (
                 f"cimac_id,parent_sample_id,collection_event_name,sample_location,type_of_sample,type_of_primary_container,{PROTOCOL_ID_FIELD_NAME},participants.cimac_participant_id\n"
                 "CTTTPP1S1.00,SA.1.1,Baseline,---,Other,Other,10021,CTTTPP1\n"
                 "CTTTPP1S2.00,SA.1.2,Baseline,---,Other,Other,10021,CTTTPP1\n"

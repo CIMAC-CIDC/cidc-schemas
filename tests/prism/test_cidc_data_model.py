555ac7ba080187aa27c1d62912c48997653a1679
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-03-24 11:30:54 -0400

    Address PR comments

--- tests/prism/test_cidc_data_model.py
+++ tests/prism/test_cidc_data_model.py
@@ -18,6 +18,22 @@ def prism_test(request):
     return request.param
 
 
+def assert_metadata_matches(received: dict, expected: dict, upload_entries: list):
+    # Take the difference of the received patch and the expected patch.
+    # We expect these patchs to differ by upload placeholder UUID only,
+    # so the number of differences should equal the number of expected
+    # upload entries.
+    diff = DeepDiff(received, expected)
+    if "values_changed" in diff:
+        assert len(diff["values_changed"]) == len(upload_entries)
+        for key in diff["values_changed"].keys():
+            assert key.endswith("['upload_placeholder']")
+    else:
+        # For manifest uploads, we expect no artifacts, so there should
+        # be no difference between expected and received trial objects.
+        assert diff == {}
+
+
 def test_prismify(prism_test: PrismTestData):
     # Run prismify on the given test case
     patch, upload_entries, errs = prismify(*prism_test.prismify_args)
@@ -25,24 +41,15 @@ def test_prismify(prism_test: PrismTestData):
     # Ensure no errors resulted from the prismify run
     assert len(errs) == 0
 
-    # Take the difference of the received patch and the expected patch.
-    # We expect these patchs to differ by upload placeholder UUID only,
-    # so the number of differences should equal the number of expected
-    # upload entries.
-    diff = DeepDiff(patch, prism_test.prismify_patch)
-    if "values_changed" in diff:
-        assert len(diff["values_changed"]) == len(upload_entries)
-    else:
-        # For manifest uploads, we expect no artifacts, so there should
-        # be no difference between expected and received prismify patches.
-        assert diff == {}
-
     # Compare the received upload entries with the expected upload entries.
     # These should differ by upload placeholder UUID only.
     for received, expected in zip(upload_entries, prism_test.upload_entries):
         assert received.local_path == expected.local_path
         assert received.gs_key == expected.gs_key
         assert received.metadata_availability == expected.metadata_availability
+
+    # Compare the received and expected prism metadata patches
+    assert_metadata_matches(patch, prism_test.prismify_patch, upload_entries)
 
 
 def test_merge_patch_into_trial(prism_test: PrismTestData):
@@ -58,14 +65,7 @@ def test_merge_patch_into_trial(prism_test: PrismTestData):
     validator = load_and_validate_schema("clinical_trial.json", return_validator=True)
     validator.validate(result)
 
-    # Ensure the merge result differs only by upload placeholder UUIDs
-    diff = DeepDiff(result, prism_test.target_trial)
-    if "values_changed" in diff:
-        assert len(diff["values_changed"]) == len(prism_test.upload_entries)
-    else:
-        # For manifest uploads, we expect no artifacts, so there should
-        # be no difference between expected and received trial objects.
-        assert diff == {}
+    assert_metadata_matches(result, prism_test.target_trial, prism_test.upload_entries)
 
 
 def test_merge_artifacts(prism_test: PrismTestData):
@@ -98,10 +98,16 @@ def test_merge_artifacts(prism_test: PrismTestData):
     # First, make an alias `root` for `patch`, because `deepdiff.grep` generates dictionary
     # paths like "root['key1']['key2']['upload_placeholder']".
     root = patch
+    placeholder_key = "['upload_placeholder']"
     for uuid, artifact in uuids_and_artifacts:
         # Get the path in the *original* patch to the placeholder uuid.
         paths = (prism_test.prismify_patch | grep(uuid))["matched_values"]
-        path = paths.pop().replace("['upload_placeholder']", "")
+        assert len(paths) == 1, "UUID should only occur once in a metadata patch"
+        path = paths.pop()
+        assert path.endswith(
+            placeholder_key
+        ), f"UUID values should have key {placeholder_key}"
+        path = path[: -len(placeholder_key)]
 
         # Ensure that evaluating this same path in the *new* patch gets the expected
         # artifact metadata dictionary.

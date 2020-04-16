5c492b6be81c49daba220a2793e8ec0ea1243e64
Pavel Trukhanov <pavel@ds.dfci.harvard.edu>
2020-04-16 18:51:23 -0400

    attempt to simplify patch_feedback

--- cidc_schemas/prism/patch_feedback.py
+++ cidc_schemas/prism/patch_feedback.py
@@ -1,20 +1,29 @@
 from typing import List, Union
 from collections import Counter, defaultdict
+from functools import wraps
 
 from .constants import SUPPORTED_MANIFESTS
 
 
+def _gen_to_list(f):
+    @wraps(f)
+    def inner(*a, **kw):
+        return [item for item in f(*a, **kw)]
+
+    return inner
+
+
 def _samples_extra_id(samples, extra_id_name):
     ids = set([s.get(extra_id_name) for s in samples])
-    if ids.difference([None]):  # if we have some values for this id prop at all
-        if len(ids) != len(samples):
-            if len(ids) == 1:
-                return f"Found the same {extra_id_name} {ids.pop()!r} for all {len(samples)} samples"
-            else:
-                return f"Found only {len(ids)} different {extra_id_name} for {len(samples)} samples"
+    if len(ids) != len(samples):
+        if len(ids) == 1:
+            return f"Found the same {extra_id_name} {ids.pop()!r} for all {len(samples)} samples"
+        else:
+            return f"Found only {len(ids)} different {extra_id_name} for {len(samples)} samples"
 
 
-def format_new_samples(new_samples):
+@_gen_to_list
+def _format_new_samples(new_samples):
 
     spp = Counter([s["cimac_participant_id"] for s in new_samples])
     spp_min = min(spp.values())
@@ -35,12 +44,13 @@ def format_new_samples(new_samples):
 
     cc = Counter([s["collection_event_name"] for s in new_samples]).items()
     if len(cc) == 1:
-        yield f"Collection event for all {len(new_samples)} - {new_samples[0]['collection_event_name']}"
+        yield f"collection_event_name event for all {len(new_samples)} - {cc[0]['collection_event_name']}"
     else:
         yield f"{len(cc)} collection events"
         yield [f"{count} {tp}'s" for tp, count in cc]
 
 
+@_gen_to_list
 def patch_feedback(
     template_type: str, patch: dict, current_md: dict
 ) -> List[Union[List, str]]:
@@ -53,64 +63,92 @@ def patch_feedback(
 
     """
 
-    res = []
-    if template_type in SUPPORTED_MANIFESTS:
+    if template_type not in SUPPORTED_MANIFESTS:
+        return []
 
-        shipment = patch["shipments"][0]
-        trial_id = patch["protocol_identifier"]
-        participants = {p["cimac_participant_id"]: p for p in patch["participants"]}
-        samples = [dict(s, **p) for p in participants.values() for s in p["samples"]]
+    shipment = patch["shipments"][0]
+    trial_id = patch["protocol_identifier"]
+    participants = {p["cimac_participant_id"]: p for p in patch["participants"]}
+    samples = [
+        dict(s, cimac_participant_id=cpid)
+        for cpid, p in participants.items()
+        for s in p["samples"]
+    ]
 
-        res.append(
-            f"{len(samples)} samples from {len(participants)} participants in "
-            f"manifest {shipment['manifest_id']!r} for {trial_id}/{shipment['assay_type']}"
+    yield (
+        f"{len(samples)} samples from {len(participants)} participants in "
+        f"manifest {shipment['manifest_id']!r} for {trial_id}/{shipment['assay_type']}"
+    )
+
+    new_ps_ids = set(participants).difference(current_md)
+
+    yield _format_new_participants({id: participants[id] for id in new_ps_ids})
+
+    upd_ps_ids = set(participants).intersection(current_md)
+    if upd_ps_ids:
+        existing_samples = {
+            s["cimac_id"]: dict(s, cimac_participant_id=cpid)
+            for cpid, p in current_md.items()
+            for s in p["samples"]
+        }
+
+        yield _format_updated_participants(
+            {id: participants[id] for id in upd_ps_ids}, existing_samples
         )
 
-        new_ps = set(participants).difference(current_md)
 
-        new_ps_clause = [f"adds {len(new_ps)} new participants"]
-        res.append(new_ps_clause)
+@_gen_to_list
+def _format_new_participants(new_ps: list) -> List[Union[List, str]]:
+    yield f"adds {len(new_ps)} new participants"
 
-        if new_ps:
-            new_samples = [
-                dict(s, **p)
-                for cpid, p in participants.items()
-                for s in p["samples"]
-                if cpid in new_ps
-            ]
+    if new_ps:
+        new_samples = [
+            dict(s, cimac_participant_id=cpid)
+            for cpid, p in new_ps.items()
+            for s in p["samples"]
+        ]
 
-            new_ps_clause.append([out for out in format_new_samples(new_samples)])
+        new_samples_warn = _format_new_samples(new_samples)
+        if new_samples_warn:
+            yield new_samples_warn
 
-        upd_ps = set(participants).intersection(current_md)
-        if upd_ps:
-            upd_ps_clause = [f"updates {len(upd_ps)} existing participants"]
-            res.append(upd_ps_clause)
 
-            existing_samples = {
-                s["cimac_id"]: dict(s, **p)
-                for p in current_md.values()
-                for s in p["samples"]
-            }
+@_gen_to_list
+def _format_updated_participants(upd_ps: list) -> List[Union[List, str]]:
+    yield f"updates {len(upd_ps)} existing participants"
 
-            new_samples, upd_samples = [], []
-            for cpid in upd_ps:
-                p = participants[cpid]
+    new_samples, upd_samples = [], []
+    for cpid, p in upd_ps.items():
+        for s in p["samples"]:
+            s = dict(s, cimac_participant_id=cpid)
+            if s["cimac_id"] in existing_samples:
+                upd_samples.append(s)
+            else:
+                new_samples.append(s)
 
-                existing_samples = []
-                for s in p["samples"]:
-                    s = dict(s, **p)
-                    if s["cimac_id"] in existing_samples:
-                        upd_samples.append(s)
-                    else:
-                        new_samples.append(s)
+    if new_samples:
+        new_samples_warn = _format_new_samples(new_samples)
+        if new_samples_warn:
+            yield new_samples_warn
 
-            if new_samples:
-                upd_ps_clause.append([out for out in format_new_samples(new_samples)])
+    if upd_samples:
+        upd_samples_warn = _format_upd_samples(upd_samples)
+        if upd_samples_warn:
+            yield upd_samples_warn
 
-            if upd_samples:
-                upd_ps_clause.append(
-                    f"with {len(upd_samples)} updated existing samples"
-                )
-                # TODO
 
-    return res
+@_gen_to_list
+def _format_upd_samples(upd_samples):
+    yield f"with {len(upd_samples)} updated existing samples"
+
+    # #TODO
+    # changes_count = 0
+    # for s in upd_samples:
+    #     e = existing_samples[s["cimac_id"]]
+    #     if s != e:
+    #         changes_count += 1
+    #         print(f'\t\t\tdiff for {s["cimac_id"]}:')
+    #         for k in s:
+    #             if s[k] != e[k]:
+    #                 print(f"\t\t\t\t {k!r} was {e[k]} updated to {s[k]}")
+    # print(f"\t\t\tand {len(upd_samples)-changes_count} unchanged")

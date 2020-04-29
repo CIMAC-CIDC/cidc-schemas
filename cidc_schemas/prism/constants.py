510b3b3410d9c5e15a1c8192ed77e2d9d98d5830
Pavel Trukhanov <pavel@ds.dfci.harvard.edu>
2020-04-29 18:42:30 -0400

    rna tumor tissue template and rna level1 analysis

--- cidc_schemas/prism/constants.py
+++ cidc_schemas/prism/constants.py
@@ -21,12 +21,13 @@ SUPPORTED_SHIPPING_MANIFESTS = [
     "normal_blood_dna",
     "normal_tissue_dna",
     "tumor_tissue_dna",
+    "tumor_tissue_rna",
     "h_and_e",
 ]
 # weird non shipping manifest
 SUPPORTED_WEIRD_MANIFESTS = ["tumor_normal_pairing"]
 SUPPORTED_MANIFESTS = SUPPORTED_SHIPPING_MANIFESTS + SUPPORTED_WEIRD_MANIFESTS
 
-SUPPORTED_ANALYSES = ["cytof_analysis", "wes_analysis", "rnaseq_analysis"]
+SUPPORTED_ANALYSES = ["cytof_analysis", "wes_analysis", "rna_level1_analysis"]
 
 SUPPORTED_TEMPLATES = SUPPORTED_ASSAYS + SUPPORTED_MANIFESTS + SUPPORTED_ANALYSES

43cbcda214ac7d42934e237f79168656d419af93
Roshni Biswas <rbiswas@ds.dfci.harvard.edu>
2020-09-03 22:32:02 -0400

    two reports directories

--- tests/prism/cidc_test_data/analysis_data.py
+++ tests/prism/cidc_test_data/analysis_data.py
@@ -434,7 +434,7 @@ def wes_analysis() -> PrismTestData:
             metadata_availability=None,
         ),
         LocalFileUploadEntry(
-            local_path="analysis/report/somatic_variants/tumor_mutational_burden.tsv",
+            local_path="analysis/report2/somatic_variants/tumor_mutational_burden.tsv",
             gs_key="test_prism_trial_id/wes/run_1/analysis/tumor_mutational_burden.tsv",
             upload_placeholder="d47271fb-e2c7-5436-cafe-5cf84bc72bf5",
             metadata_availability=None,
@@ -674,7 +674,7 @@ def wes_analysis() -> PrismTestData:
             metadata_availability=None,
         ),
         LocalFileUploadEntry(
-            local_path="analysis/report/somatic_variants/tumor_mutational_burden.tsv",
+            local_path="analysis/report2/somatic_variants/tumor_mutational_burden.tsv",
             gs_key="test_prism_trial_id/wes/run_2/analysis/tumor_mutational_burden.tsv",
             upload_placeholder="66824763-fb9f-58b4-c7c4-8175759933f5",
             metadata_availability=None,

f0180635b603fa3fc8811caf7039d4248698badf
Roshni Biswas <rbiswas@ds.dfci.harvard.edu>
2020-12-02 17:52:44 -0500

    Fix TCR analysis example data

--- tests/prism/cidc_test_data/analysis_data.py
+++ tests/prism/cidc_test_data/analysis_data.py
@@ -1366,7 +1366,7 @@ def tcr_analysis() -> PrismTestData:
             metadata_availability=False,
         ),
         LocalFileUploadEntry(
-            local_path="9204_trial.tar.gz",
+            local_path="9204_report.tar.gz",
             gs_key="test_prism_trial_id/tcr_analysis/XYZ/report_trial.tar.gz",
             upload_placeholder="912f4bae-bca8-42f6-a3b7-cb4db27b2e24",
             metadata_availability=False,

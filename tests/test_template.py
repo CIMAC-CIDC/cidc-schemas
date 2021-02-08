c3bc8f5fb1658bcfe16017f3f525f3aaf2b0df8e
SC Van Nostrand <scvannost@gmail.com>
2021-02-08 12:04:19 -0500

    black

--- tests/test_template.py
+++ tests/test_template.py
@@ -531,7 +531,9 @@ def test_convert_api_to_template_rna():
         },
     }
 
-    assay_schema = _load_dont_validate_schema("assays/components/ngs/rna/rna_analysis.json")
+    assay_schema = _load_dont_validate_schema(
+        "assays/components/ngs/rna/rna_analysis.json"
+    )
     rna_output = _convert_api_to_template("rna", rna_api, assay_schema)
     assert DeepDiff(rna_json, rna_output) == {}
 
